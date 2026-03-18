e749bdde4c3040ac7e736f1b835f50a53150bb67
Eoin Geoghegan <152919198+eoingeoghegan@users.noreply.github.com>
2026-03-18 15:14:19 +0000

    Updated dotenv sever coomented process.exit for render

--- src/server.js
+++ src/server.js
@@ -67,7 +67,7 @@ async function init() {
   const result = dotenv.config();
     if (result.error) {
     console.log(result.error.message);
-    process.exit(1);
+    //process.exit(1);
   }
 
   /**
